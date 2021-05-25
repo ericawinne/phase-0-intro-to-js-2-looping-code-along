a6d493b53b2db830d5766ff573e5d96cec607266
Erica Winne <erica.winne@powerhrg.com>
2021-05-25 10:39:36 -0400

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thanks = []
+    for (let p = 0; p < names.length; p++){
+        thanks.push(`Thank you, ${names [p]}, for the wonderful ${event} gift!`)
+    }
+    return thanks 
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--
+    }
+}
+
